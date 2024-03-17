a1ba1f1600c6ed39f8f37ccf8852a2d796dd981e
Dmytro Storchak <dmitriystorchak98@gmail.com>
2024-03-17 16:52:49 +0200

    fixed: rename variables, improve the logic

--- Task1.py
+++ Task1.py
@@ -1,54 +1,59 @@
 class Book:
     """Model a book"""
 
-    def __init__(self, author, book_name, year, genre):
+    def __init__(self, author, name, year, genre):
         """Initialize the attributes that describe the book"""
         self.author = author
-        self.book_name = book_name
+        self.name = name
         self.year = year
         self.genre = genre
-        self.review_list = []
+        self.reviews = []
 
     def add_review(self, review):
         """Add new review in the list"""
-        self.review_list.append(review)
+        self.reviews.append(review)
 
     def __str__(self):
         """Print book description"""
-        print_book = f"'{self.book_name}': {self.author}, {self.year}, {self.genre}"
+        result = f"'{self.name}': {self.author}, {self.year}, {self.genre}"
 
-        if self.review_list:
-            return print_book + f", reviews: {self.review_list}"
-        else:
-            return print_book + ", reviews: no reviews yet"
+        if not self.reviews:
+            return result + ", reviews: no reviews yet"
+
+        for review in self.reviews:
+            result += f"\nReview: {review}"
+
+        return result
 
     def __repr__(self):
-        """Print book description"""
-        return f"({self.author}, {self.book_name}, {self.year}, {self.genre})"
+        return (f"Book(author={self.author}, "
+                f"name={self.name}, "
+                f"year={self.year}, "
+                f"genre={self.genre}, "
+                f"reviews={self.reviews})")
 
 
 class Review:
     """Describes the book review"""
 
-    def __init__(self, review_text, rating, author_review):
+    def __init__(self, text, rating, author):
         """
         Initialize review attribute
         The rating cannot be more than 100
         """
-        self.review_text = review_text
+        self.text = text
         self.rating = rating
-        self.author_review = author_review
+        self.author = author
 
         if rating > 100:
             self.rating = 100
 
     def __str__(self):
         """Print review description"""
-        return f"'{self.review_text}', user rating - {self.rating}, reviewer {self.author_review}"
+        return f"'{self.text}', user rating - {self.rating}, reviewer {self.author}"
 
     def __repr__(self):
-        """Print review description in list"""
-        return f"('{self.review_text}', {self.rating}/100, reviewer {self.author_review})"
+        return f"Review(text={self.text}, rating={self.rating}, author={self.author})"
 
 
 book1 = Book("Stephen King", "The Shining", 1977, "horror")
--- Task4.py
+++ Task4.py
@@ -1,75 +1,107 @@
 class Car:
     """Model and describe the car"""
 
-    def __init__(self, make, model, year, color):
+    def __init__(self, make, model, year, color, vin):
         """Initiate the attributes that describe the car"""
         self.make = make
         self.model = model
         self.year = year
         self.color = color
+        self.vin = vin
 
     def __str__(self):
         """Show car describe """
-        return f"{self.make} {self.model}, {self.year}, {self.color}"
+        return f"{self.make} {self.model}, {self.year}, {self.color}, vin - '{self.vin}'"
 
     def __repr__(self):
-        """Show car description in the list"""
-        return f"({self.make} {self.model}, {self.year}, {self.color})"
+        return (f"Car(make={self.make}, "
+                f"model={self.model}, "
+                f"year={self.year}, "
+                f"color={self.color}, "
+                f"vin={self.vin})")
 
 
-class CarDealership:
+class Dealership:
     """Model a car dealership"""
 
     def __init__(self):
-        """Create a list of car"""
-        self.car_list = []
-        self.reserve_car = []
+        """Create a car dictionary where vin is the key"""
+        self.lots = {}
 
-    def add_car(self, *new_cars):
-        """Add a new car"""
-        for car in new_cars:
-            self.car_list.append(car)
+    def add(self, *cars):
+        """Add a new cars"""
+        if cars is None or len(cars) == 0:
+            print("Please provide cars")
+            return
 
-    def sell_car(self, car):
+        for car in cars:
+            self.lots.update({car.vin: Lot(car)})
+
+    def sell(self, vin):
         """Sell a car"""
-        if car in self.car_list:
-            self.car_list.remove(car)
-        else:
-            print("There is no such car")
+        lot = self.lots.pop(vin)
 
-    def reserved_car(self, car):
-        """Reserve a car, not available for sale"""
-        if car in self.car_list:
-            car_index = self.car_list.index(car)
-            r_car = self.car_list.pop(car_index)
-            self.reserve_car.append(r_car)
-        else:
+        if lot is None:
             print("There is no such car")
+            return
+
+        if lot.reserved:
+            print("Cannot sell reserved car")
+            return
+
+        print(f"Car {lot.car} is successfully sold")
+
+    def reserve(self, vin):
+        """Reserve a car"""
+        lot = self.lots.get(vin)
+        if lot is None:
+            print("There is no such car")
+            return
+
+        lot.reserved = True
+        print("Car successfully reserved")
 
     def __str__(self):
         """Show car lists"""
-        return f"Available cars: {self.car_list}\nReserved cars: {self.reserve_car}"
+        result = "Available cars:"
+        for car in self.lots.values():
+            result += f"\n{car}"
+        return result
 
     def __repr__(self):
-        """Show car lists"""
-        return f"({self.car_list}, {self.reserve_car})"
+        return f"Dealership(lots={self.lots})"
 
 
-car1 = Car("Subaru", "Outback", 2017, "blue")
-car2 = Car("Subaru", "Outback", 2015, "black")
-car3 = Car("Nissan", "Rogue", 2019, "blue")
-car4 = Car("Jeep", "Cherokee", 2016, "red")
-car5 = Car("Jeep", "Cherokee", 2017, "white")
+class Lot:
+    """Model a lot with the ability to reserve a car"""
+    def __init__(self, car):
+        """Initiate the attributes"""
+        self.car = car
+        self.reserved = False
 
-car_dealership = CarDealership()
-car_dealership.add_car(car1, car2, car3, car4, car5)
+    def __str__(self):
+        """Show car whether it is reserved"""
+        return f"{self.car}, reserve: {self.reserved}"
+
+    def __repr__(self):
+        return f"Lot(car={self.car}, reserved={self.reserved})"
+
+
+car1 = Car("Subaru", "Outback", 2017, "blue", "123")
+car2 = Car("Subaru", "Outback", 2015, "black", "234")
+car3 = Car("Nissan", "Rogue", 2019, "blue", "345")
+car4 = Car("Jeep", "Cherokee", 2016, "red", "567")
+car5 = Car("Jeep", "Cherokee", 2017, "white", "777")
+
+car_dealership = Dealership()
+car_dealership.add(car1, car2, car3, car4, car5)
 print(car_dealership)
 
 print()
-car_dealership.reserved_car(car2)
+car_dealership.reserve(car2.vin)
 print(car_dealership)
 
 print()
-car_dealership.sell_car(car1)
-car_dealership.sell_car(car5)
+car_dealership.sell(car1.vin)
+car_dealership.sell(car5.vin)
 print(car_dealership)
